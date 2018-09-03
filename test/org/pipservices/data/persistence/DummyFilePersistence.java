a81b68cf33ee0025ba94d2332888b252c13327a7
EgorNuzhnykh <nuzhnykhev@gmail.com>
2018-09-03 10:02:02 +0300

    Minor changes

--- test/org/pipservices/data/persistence/DummyFilePersistence.java
+++ test/org/pipservices/data/persistence/DummyFilePersistence.java
@@ -3,11 +3,8 @@ package org.pipservices.data.persistence;
 import org.pipservices.commons.config.*;
 import org.pipservices.commons.errors.*;
 import org.pipservices.data.*;
-<<<<<<< HEAD:test/org/pipservices/data/file/DummyFilePersistence.java
-import org.pipservices.data.memory.*;
 import org.pipservices.data.persistence.JsonFilePersister;
-=======
->>>>>>> 5aed1309012c035f27fcea996e6becb69cbf2522:test/org/pipservices/data/persistence/DummyFilePersistence.java
+
 
 public class DummyFilePersistence extends DummyMemoryPersistence {
 	protected JsonFilePersister<Dummy> _persister;
